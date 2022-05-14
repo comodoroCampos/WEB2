a228c4ee9138be2f0b1affd99206a482594c4807
joaquin fernandez <47857926+jfernalv106@users.noreply.github.com>
2022-05-13 20:56:50 -0400

    services

---
+++ ECOMMERCE/WEBECOMMERCE/src/app/venta/input-venta/input-venta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-venta',
+  templateUrl: './input-venta.component.html',
+  styleUrls: ['./input-venta.component.css']
+})
+export class InputVentaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- ECOMMERCE/WEBECOMMERCE/src/app/venta/venta.module.ts
+++ ECOMMERCE/WEBECOMMERCE/src/app/venta/venta.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputVentaComponent } from './input-venta/input-venta.component';
+import { ResultadosComponent } from './resultados/resultados.component';
+import { BuscarComponent } from './buscar/buscar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InputVentaComponent,
+    ResultadosComponent,
+    BuscarComponent
+  ],
   imports: [
     CommonModule
   ]
