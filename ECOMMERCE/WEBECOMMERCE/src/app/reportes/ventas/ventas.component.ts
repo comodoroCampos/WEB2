d7004c1546534d3b8244aba843d206e6d00ffa79
joaquin fernandez <47857926+jfernalv106@users.noreply.github.com>
2022-05-16 12:55:29 -0400

    weas

--- ECOMMERCE/WEBECOMMERCE/src/app/reportes/ventas/ventas.component.ts
+++ ECOMMERCE/WEBECOMMERCE/src/app/reportes/ventas/ventas.component.ts
@@ -3,31 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ventas',
   templateUrl: './ventas.component.html',
-  styleUrls: ['./ventas.component.css']
+  styleUrls: ['./ventas.component.css'],
 })
 export class VentasComponent implements OnInit {
-  fecha_desde:Date=new Date(); 
-  fecha_hasta:Date=new Date(); 
-  empleado:string = '';
-  
+  fecha_desde: Date;
+  fecha_hasta: Date;
+  disable_hasta: boolean = true;
+  empleado: string = '';
+
   constructor() {
-    this.fecha_desde=new Date();
-    this.fecha_hasta=new Date();
-    this.empleado='';
-   }
-
-  ngOnInit(): void {
+    this.fecha_desde = new Date();
+    this.fecha_hasta = new Date();
+    this.empleado = '';
   }
-selecionaFecha(){
-  console.log(this.fecha_desde);
-  console.log(this.fecha_hasta);
-}
 
-  buscar(){
+  ngOnInit(): void {}
+  selecionaFecha() {
+    this.disable_hasta = false;
+    if (this.fecha_desde < this.fecha_hasta) {
+      this.disable_hasta = false;
+    }
+    console.log(this.fecha_desde);
+    console.log(this.fecha_hasta);
+  }
+
+  buscar() {
     console.log(this.fecha_desde);
     console.log(this.fecha_hasta);
     console.log(this.empleado);
-
-  };
-
+  }
 }
