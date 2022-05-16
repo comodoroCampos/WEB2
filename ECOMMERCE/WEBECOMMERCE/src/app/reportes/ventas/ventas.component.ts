686aabd43c58f9937617f6799dc1dc422c5b02b0
joaquin fernandez <47857926+jfernalv106@users.noreply.github.com>
2022-05-16 12:47:28 -0400

    weas

--- ECOMMERCE/WEBECOMMERCE/src/app/reportes/ventas/ventas.component.ts
+++ ECOMMERCE/WEBECOMMERCE/src/app/reportes/ventas/ventas.component.ts
@@ -6,14 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ventas.component.css']
 })
 export class VentasComponent implements OnInit {
-
-  constructor() { }
+  fecha_desde:Date=new Date(); 
+  fecha_hasta:Date=new Date(); 
+  empleado:string = '';
+  
+  constructor() {
+    this.fecha_desde=new Date();
+    this.fecha_hasta=new Date();
+    this.empleado='';
+   }
 
   ngOnInit(): void {
   }
+selecionaFecha(){
+  console.log(this.fecha_desde);
+  console.log(this.fecha_hasta);
+}
 
-  fecha:Date=new Date(); 
+  buscar(){
+    console.log(this.fecha_desde);
+    console.log(this.fecha_hasta);
+    console.log(this.empleado);
 
-  buscar(){};
+  };
 
 }
