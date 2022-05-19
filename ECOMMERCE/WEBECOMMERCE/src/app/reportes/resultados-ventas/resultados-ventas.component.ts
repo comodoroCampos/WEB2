d140ff4df6ad0aa976913b75f8455e02d400c94d
comodoroCampos <comodoro1campos@gmail.com>
2022-05-19 13:37:02 -0400

    a

--- ECOMMERCE/WEBECOMMERCE/src/app/reportes/resultados-ventas/resultados-ventas.component.ts
+++ ECOMMERCE/WEBECOMMERCE/src/app/reportes/resultados-ventas/resultados-ventas.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Venta } from 'src/app/interfaces/interface';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-resultados-ventas',
@@ -18,10 +19,28 @@ export class ResultadosVentasComponent implements OnInit {
     'usuario',
     'anular'
   ];
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   eliminar(venta: Venta) {
     console.log(venta);
+  }
+
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+      const worksheet = xlsx.utils.json_to_sheet(this.ventas);
+      const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+      this.saveAsExcelFile(excelBuffer, "products");
+    });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
   }
 }
