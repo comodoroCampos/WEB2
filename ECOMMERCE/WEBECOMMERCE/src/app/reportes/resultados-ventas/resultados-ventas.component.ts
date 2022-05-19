c5fbe79cff7ae2ac8d2c61935d97b9c8c91e63e3
joaquin fernandez <47857926+jfernalv106@users.noreply.github.com>
2022-05-19 17:31:00 -0400

    weas

--- ECOMMERCE/WEBECOMMERCE/src/app/reportes/resultados-ventas/resultados-ventas.component.ts
+++ ECOMMERCE/WEBECOMMERCE/src/app/reportes/resultados-ventas/resultados-ventas.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Venta } from 'src/app/interfaces/interface';
+import { ReporteVentas, Venta } from 'src/app/interfaces/interface';
 import * as FileSaver from 'file-saver';
 import * as moment from 'moment';
 
@@ -12,38 +12,57 @@ export class ResultadosVentasComponent implements OnInit {
   @Input() ventas: Venta[] = [];
   @Input() ventasAux: Venta[] = [];
 
+  reporteVenta: ReporteVentas[] = [];
+
   displayedColumns: string[] = [
     'cantidad',
     'producto',
     'fecha',
     'tipo',
-    'usuario'
+    'usuario',
   ];
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
   eliminar(venta: Venta) {
     console.log(venta);
   }
 
-  formatoFecha:string = (moment(new Date())).format('YYYY-MM-DD HH:mm');
+  formatoFecha: string = moment(new Date()).format('YYYY-MM-DD HH:mm');
 
   exportExcel() {
     console.log(this.formatoFecha);
-    import("xlsx").then(xlsx => {
-      const worksheet = xlsx.utils.json_to_sheet(this.ventas);
-      const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-      const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
-      this.saveAsExcelFile(excelBuffer, "products");
+    this.reporteVenta = [];
+    for (let venta of this.ventas) {
+      this.reporteVenta.push({
+        cantidad: venta.cantidad,
+        producto: venta.producto,
+        fecha: moment(venta.fecha).format('YYYY-MM-DD HH:mm'),
+        tipo: venta.tipo,
+        usuario: venta.usuario,
+      });
+    }
+    import('xlsx').then((xlsx) => {
+      const worksheet = xlsx.utils.json_to_sheet(this.reporteVenta);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      });
+      this.saveAsExcelFile(excelBuffer, 'products');
     });
   }
 
   saveAsExcelFile(buffer: any, fileName: string): void {
-    let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_TYPE =
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
     let EXCEL_EXTENSION = '.xlsx';
     const data: Blob = new Blob([buffer], {
-      type: EXCEL_TYPE
+      type: EXCEL_TYPE,
     });
-    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+    FileSaver.saveAs(
+      data,
+      fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION
+    );
   }
 }
