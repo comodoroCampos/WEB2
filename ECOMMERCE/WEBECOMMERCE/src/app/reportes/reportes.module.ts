016f0a9370ad16e2d8a9d5d84756b689c62e57ed
comodoroCampos <comodoro1campos@gmail.com>
2022-05-30 17:50:12 -0400

    a

--- ECOMMERCE/WEBECOMMERCE/src/app/reportes/reportes.module.ts
+++ ECOMMERCE/WEBECOMMERCE/src/app/reportes/reportes.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { VentasComponent } from './ventas/ventas.component';
 import { InventarioComponent } from './inventario/inventario.component';
 import { DespachoComponent } from './despacho/despacho.component';
-import { ProveedorComponent } from './proveedor/proveedor.component';
-import { PostVentaComponent } from './post-venta/post-venta.component';
 import { MaterialModule } from '../material/material.module';
 import { PrimengModule } from '../primeng/primeng.module';
 import { ResultadosVentasComponent } from './resultados-ventas/resultados-ventas.component';
@@ -16,8 +14,6 @@ import { ResultadosVentasComponent } from './resultados-ventas/resultados-ventas
     VentasComponent,
     InventarioComponent,
     DespachoComponent,
-    ProveedorComponent,
-    PostVentaComponent,
     ResultadosVentasComponent
   ],
   imports: [
@@ -29,8 +25,6 @@ import { ResultadosVentasComponent } from './resultados-ventas/resultados-ventas
     VentasComponent,
     InventarioComponent,
     DespachoComponent,
-    ProveedorComponent,
-    PostVentaComponent,
     ResultadosVentasComponent
   ]
 })
