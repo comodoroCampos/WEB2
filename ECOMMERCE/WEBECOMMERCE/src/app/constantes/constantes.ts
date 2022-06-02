7bc7fcdedd64d094137e1ed71ccdb653ef7f57b8
comodoroCampos <comodoro1campos@gmail.com>
2022-06-02 15:34:51 -0400

    a

--- ECOMMERCE/WEBECOMMERCE/src/app/constantes/constantes.ts
+++ ECOMMERCE/WEBECOMMERCE/src/app/constantes/constantes.ts
@@ -1,3 +1,3 @@
 //export const URL_SERVICIOS = 'http://52.4.72.215';
-//export const URL_SERVICIOS = 'http://192.168.86.220';
-export const URL_SERVICIOS = 'http://192.168.0.6';
+export const URL_SERVICIOS = 'http://192.168.86.220';
+//export const URL_SERVICIOS = 'http://192.168.0.6';
