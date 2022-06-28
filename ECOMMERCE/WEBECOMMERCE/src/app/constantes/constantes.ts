314b1a17da4eb35c4df57be37921492e5d8458ec
comodoroCampos <comodoro1campos@gmail.com>
2022-06-28 18:43:04 -0400

    a

--- ECOMMERCE/WEBECOMMERCE/src/app/constantes/constantes.ts
+++ ECOMMERCE/WEBECOMMERCE/src/app/constantes/constantes.ts
@@ -1,4 +1,5 @@
 //export const URL_SERVICIOS = 'http://52.4.72.215';
 //export const URL_SERVICIOS = 'http://192.168.86.220';
-export const URL_SERVICIOS = 'http://192.168.0.6';
+//export const URL_SERVICIOS = 'http://192.168.0.6';
 //export const URL_SERVICIOS = 'http://testrestjuaco106.ddns.net';
+export const URL_SERVICIOS = 'http://3.226.219.119';
